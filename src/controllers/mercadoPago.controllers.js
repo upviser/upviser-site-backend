5db3bdb798db1c1f1fbd523ba399f6a1e013459f
upvisor <admin@upvisor.cl>
2025-06-26 13:55:46 -0400

    actualization general

--- src/controllers/mercadoPago.controllers.js
+++ src/controllers/mercadoPago.controllers.js
@@ -1,4 +1,4 @@
-import { MercadoPagoConfig, Payment } from 'mercadopago';
+import { MercadoPagoConfig, Payment, PreApproval } from 'mercadopago';
 import Paym from '../models/Payment.js'
 
 export const createPay = async (req, res) => {
@@ -18,4 +18,31 @@ export const createPay = async (req, res) => {
         return res.status(500).json({message: error.message})
     }
     
+}
+
+export const createSuscribe = async (req, res) => {
+    try {
+        const paymentData = await Paym.findOne()
+        const client = new MercadoPagoConfig({ accessToken: paymentData.mercadoPagoSuscription.accessToken, options: { timeout: 5000 } });
+        const preapproval = new PreApproval(client);
+        const body = {
+            reason: `Suscripción ${req.body.frequency}`,
+            payer_email: req.body.email,
+            card_token_id: req.body.cardToken,
+            auto_recurring: {
+                frequency: 1,
+                frequency_type: req.body.frequency === 'Mensual' ? 'months' : 'years',
+                transaction_amount: req.body.price,
+                currency_id: 'CLP',
+                start_date: new Date()
+            },
+            back_url: `${process.env.WEB_URL}/gracias-por-comprar`,
+            status: 'authorized'
+        };
+
+        const response = await preapproval.create({ body });
+        return res.json(response.body);
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }
 }
--- src/models/Payment.js
+++ src/models/Payment.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose'
 const PaymentSchema = new mongoose.Schema({
     transbank: { active: { type: Boolean }, commerceCode: { type: String }, apiKey: { type: String } },
     mercadoPago: { active: { type: Boolean }, accessToken: { type: String }, publicKey: { type: String } },
-    mercadoPagoPro: { active: { type: Boolean }, accessToken: { type: String }, publicKey: { type: String } }
+    mercadoPagoPro: { active: { type: Boolean }, accessToken: { type: String }, publicKey: { type: String } },
+    suscription: { active: { type: Boolean }, accessToken: { type: String }, publicKey: { type: String } }
 }, {
     timestamps: true
 })
