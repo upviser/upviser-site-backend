dc1243f0b41fa845cab6ccffaf01abe737177ede
upvisor <admin@upvisor.cl>
2025-07-21 19:37:09 -0400

    actualization general

--- src/controllers/webhook.controllers.js
+++ src/controllers/webhook.controllers.js
@@ -1028,6 +1028,7 @@ export const callbackFacebook = async (req, res) => {
     
         res.status(200).json({ success: 'OK' });
     } catch (error) {
+        console.log(error)
         return res.status(500).json({message: error.message})
     }
 }
