efaf181fa096915d5a3f577e5f3cc68eea61094b
upvisor <admin@upvisor.cl>
2025-07-21 19:18:52 -0400

    actualization general

--- src/controllers/messengerMessages.controllers.js
+++ src/controllers/messengerMessages.controllers.js
@@ -175,8 +175,6 @@ export const DisconnectFacebook = async (req, res) => {
         });
 
         await Integration.findOneAndUpdate({ messengerToken: '', idPage: '', idInstagram: '', userAccessToken: '' })
-        const shopLogin = await ShopLogin.findOne({ type: 'Administrador' }).lean()
-        await axios.post(`${process.env.MAIN_API_URL}/user`, { email: shopLogin.email, api: process.env.NEXT_PUBLIC_API_URL, idPage: '', idInstagram: '' })
         return res.json({ success: 'OK' })
     } catch (error) {
         return res.status(500).json({ error: error });
