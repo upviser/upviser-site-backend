3207c9500269cf5cc64f5b2262c792c22db8ebd1
upvisor <admin@upvisor.cl>
2025-08-07 14:56:58 -0400

    actualization general

--- src/controllers/transbank.controllers.js
+++ src/controllers/transbank.controllers.js
@@ -12,8 +12,7 @@ export const create = asyncHandler(async function (req, res) {
   const buyOrder = `${storeData.name !== '' ? storeData.name : 'WEB'}-${pay + 1001}`
   const sessionId = `P-${1001 + Number(pay)}`
   const payment = await Payment.findOne().lean()
-  // const createResponse = await (new WebpayPlus.Transaction(new Options(payment.transbank.commerceCode, payment.transbank.apiKey, Environment.Integration))).create(
-  const createResponse = await (new WebpayPlus.Transaction(new Options(597055555532, '579B532A7440BB0C9079DED94D31EA1615BACEB56610332264630D42D0A36B1C', Environment.Integration))).create(
+  const createResponse = await (new WebpayPlus.Transaction(new Options(payment.transbank.commerceCode, payment.transbank.apiKey, (payment.transbank.commerceCode && payment.transbank.commerceCode === '597055555532') && (payment.transbank.apiKey && payment.transbank.apiKey === '579B532A7440BB0C9079DED94D31EA1615BACEB56610332264630D42D0A36B1C') ? Environment.Integration : Environment.Production))).create(
     buyOrder,
     sessionId,
     amount,
@@ -26,8 +25,7 @@ export const commit = asyncHandler(async function (req, res) {
   try {
     let { token } = req.body
     const payment = await Payment.findOne().lean()
-    // const commitResponse = await (new WebpayPlus.Transaction(new Options(payment.transbank.commerceCode, payment.transbank.apiKey, Environment.Integration))).commit(token)
-    const commitResponse = await (new WebpayPlus.Transaction(new Options(597055555532, '579B532A7440BB0C9079DED94D31EA1615BACEB56610332264630D42D0A36B1C', Environment.Integration))).commit(token)
+    const commitResponse = await (new WebpayPlus.Transaction(new Options(payment.transbank.commerceCode, payment.transbank.apiKey, (payment.transbank.commerceCode && payment.transbank.commerceCode === '597055555532') && (payment.transbank.apiKey && payment.transbank.apiKey === '579B532A7440BB0C9079DED94D31EA1615BACEB56610332264630D42D0A36B1C') ? Environment.Integration : Environment.Production))).commit(token)
     res.send(commitResponse)
   } catch (error) {
     return res.status(204).json({message: 'Pago no realizado'})
