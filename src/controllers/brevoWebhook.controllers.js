08cfda9483baba6b18592d831a01791d3b7324a2
upvisor <admin@upvisor.cl>
2025-09-26 11:27:06 -0300

    actualization general

--- src/controllers/brevoWebhook.controllers.js
+++ src/controllers/brevoWebhook.controllers.js
@@ -1,10 +1,19 @@
 import { updateClientEmailStatusById } from '../utils/updateEmail.js'
+import User from '../models/User.js'
+import axios from 'axios';
 
 export const getStatus = async (req, res) => {
-    console.log(req.body)
     const event = req.body.event;
     const email = req.body.email
     const emailId = req.body.tags[0]
+    const senderEmail = req.body.sender_email
+
+    if (senderEmail !== process.env.BREVO_EMAIL) {
+        const user = await User.findOne({ email: senderEmail }).lean()
+        if (!user) return res.status(200).send('Webhook received');
+        await axios.post(`${user.api}/brevo-webhook`, req.body)
+        return res.status(200).send('Webhook received');
+    }
     
     if (event === 'unique_opened') {
         await updateClientEmailStatusById(email, emailId, 'unique_opened');
