5113cddef31a916e70e24bd0765987910b576dc5
upvisor <admin@upvisor.cl>
2025-10-03 22:49:42 -0300

    actualization genearl

--- src/controllers/webhook.controllers.js
+++ src/controllers/webhook.controllers.js
@@ -1185,13 +1185,18 @@ export const getMessage = async (req, res) => {
                 }
             }
         } else if (req.body?.entry && req.body.entry[0]?.changes[0]?.value?.text) {
+            console.log(req.body.entry[0]?.changes[0]?.value?.text)
             if (req.body.entry[0].id === integration.idInstagram) {
                 const sender = req.body.entry[0].changes[0].value.from?.id
+                console.log(sender)
                 const comment = req.body.entry[0].changes[0].value.text
                 const id = req.body.entry[0].id
+                console.log(id)
                 const comments = await Comment.find().lean()
+                console.log(comments)
                 const commentAutomatization = comments.find(com => comment.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").includes(com.text.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")))
                 if (commentAutomatization) {
+                    console.log(commentAutomatization)
                     const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY})
                     const response = await openai.chat.completions.create({
                         model: "gpt-4o-mini",
@@ -1208,6 +1213,7 @@ export const getMessage = async (req, res) => {
                         presence_penalty: 0,
                         store: false
                     });
+                    console.log(response)
                     await axios.post(`https://graph.instagram.com/v23.0/${id}/replies`, {
                         "message": response.choices[0].message.content
                     }, {
